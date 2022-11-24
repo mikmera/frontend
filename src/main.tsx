4b4fdca940162c5a68594e691f4e7953ac3eab22
Pariring <paring@pikokr.dev>
2022-11-24 16:12:05 +0900

    feat: pretendard

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { Routing } from './Routing'
 import { CssBaseline } from '@mui/material'
+import './global.scss'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
