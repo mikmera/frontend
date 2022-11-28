21cf930da6a4231a808e0e2b55e9089b7757177b
Pariring <paring@pikokr.dev>
2022-11-28 18:13:38 +0900

    feat: stats area

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,18 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { Routing } from './Routing'
 import { CssBaseline } from '@mui/material'
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js'
 import './global.scss'
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend)
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
--- src/routes/dex/Stats.tsx
+++ src/routes/dex/Stats.tsx
@@ -1,7 +1,52 @@
 import { Box, Grid, Paper, Typography } from '@mui/material'
 import React, { PropsWithChildren } from 'react'
+import { useCurrentDexItem } from './context'
+import { Bar } from 'react-chartjs-2'
+import { StatKey } from '~/types'
+import { ChartData, ChartOptions } from 'chart.js'
 
-const StatSection: React.FC<PropsWithChildren<{ title: string }>> = ({
+const statLabels: Record<StatKey, [string, string]> = {
+  hp: ['#EA3323', '체력'],
+  atk: ['#E18644', '공격'],
+  def: ['#F2D154', '방어'],
+  spa: ['#708FE9', '특공'],
+  spd: ['#8BC660', '특방'],
+  spe: ['#E66388', '스핏'],
+}
+
+const statOptions: ChartOptions = {
+  responsive: true,
+  indexAxis: 'y' as const,
+  plugins: {
+    legend: {
+      position: 'right',
+    },
+  },
+  maintainAspectRatio: false,
+}
+
+const StatsArea: React.FC = () => {
+  const item = useCurrentDexItem()
+
+  const data: ChartData<'bar', number[]> = React.useMemo(() => {
+    return {
+      datasets: Object.entries(statLabels).map(([key, [color, value]]) => ({
+        data: [item.pokemon.stats[key as StatKey]],
+        label: value,
+        backgroundColor: color,
+      })),
+      labels: ['스탯'],
+    }
+  }, [item.pokemon.stats])
+
+  return (
+    <Box sx={{ minHeight: 180, p: 1 }}>
+      <Bar height="100%" data={data} options={statOptions} />
+    </Box>
+  )
+}
+
+const DetailSection: React.FC<PropsWithChildren<{ title: string }>> = ({
   title,
   children,
 }) => {
@@ -19,13 +64,15 @@ export const DexStats: React.FC = () => {
   return (
     <Grid container spacing={2}>
       <Grid item xs={12} md={6} lg={4}>
-        <StatSection title="종족값 분배">ㅁㄴㅇㄹㅁㄴㅇㄹ</StatSection>
+        <DetailSection title="종족값 분배">
+          <StatsArea />
+        </DetailSection>
       </Grid>
       <Grid item xs={12} md={6} lg={4}>
-        <StatSection title="기술">ㅁㄴㅇㄹㅁㄴㅇㄹ</StatSection>
+        <DetailSection title="기술">ㅁㄴㅇㄹㅁㄴㅇㄹ</DetailSection>
       </Grid>
       <Grid item xs={12} md={6} lg={4}>
-        <StatSection title="성격">ㅁㄴㅇㄹㅁㄴㅇㄹ</StatSection>
+        <DetailSection title="성격">ㅁㄴㅇㄹㅁㄴㅇㄹ</DetailSection>
       </Grid>
       <Grid item xs={12} md={6}>
         <Paper variant="outlined" sx={{ p: 2 }}>
@@ -38,13 +85,13 @@ export const DexStats: React.FC = () => {
         </Paper>
       </Grid>
       <Grid item xs={12} md={6} lg={4}>
-        <StatSection title="테라스탈 타입">ㅁㄴㅇㄹㅁㄴㅇㄹ</StatSection>
+        <DetailSection title="테라스탈 타입">ㅁㄴㅇㄹㅁㄴㅇㄹ</DetailSection>
       </Grid>
       <Grid item xs={12} md={6} lg={4}>
-        <StatSection title="지닌 물건">ㅁㄴㅇㄹㅁㄴㅇㄹ</StatSection>
+        <DetailSection title="지닌 물건">ㅁㄴㅇㄹㅁㄴㅇㄹ</DetailSection>
       </Grid>
       <Grid item xs={12} md={6} lg={4}>
-        <StatSection title="특성">ㅁㄴㅇㄹㅁㄴㅇㄹ</StatSection>
+        <DetailSection title="특성">ㅁㄴㅇㄹㅁㄴㅇㄹ</DetailSection>
       </Grid>
     </Grid>
   )
