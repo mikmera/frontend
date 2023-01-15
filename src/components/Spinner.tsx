bc42f8709f4f04e64c6b890e6b8089c715254998
rihayoru <qtcutie@kakao.com>
2023-01-15 21:30:46 +0900

    feat: change loading spinner

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,26 +1,6 @@
 import React from 'react'
-import styles from './Spinner.module.css'
+import './swshLoading.scss'
 
 export const Spinner: React.FC = () => {
-  return (
-    <div className={styles.loader}>
-      <div className={styles['loader-inner']}>
-        <div className={styles['loader-line-wrap']}>
-          <div className={styles['loader-line']}></div>
-        </div>
-        <div className={styles['loader-line-wrap']}>
-          <div className={styles['loader-line']}></div>
-        </div>
-        <div className={styles['loader-line-wrap']}>
-          <div className={styles['loader-line']}></div>
-        </div>
-        <div className={styles['loader-line-wrap']}>
-          <div className={styles['loader-line']}></div>
-        </div>
-        <div className={styles['loader-line-wrap']}>
-          <div className={styles['loader-line']}></div>
-        </div>
-      </div>
-    </div>
-  )
+  return <div className="pokeball" />
 }
