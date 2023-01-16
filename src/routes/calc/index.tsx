255847504dcf17bf5196f22093d145a4482e15fc
rihayoru <qtcutie@kakao.com>
2023-01-16 21:18:05 +0900

    feat: base work for calculator

--- src/routes/calc/index.tsx
+++ src/routes/calc/index.tsx
@@ -1,7 +1,30 @@
 import { Box } from '@mui/material'
 import React from 'react'
 import { wrapError } from '~/components/ErrorBoundary'
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 
 export const Main: React.FC = wrapError(() => {
-  return <Box>Calc</Box>
+  const [calcMode, setCalcMode] = React.useState('')
+
+  const handleChange = (
+    event: React.MouseEvent<HTMLElement>,
+    newAlignment: string,
+  ) => {
+    setCalcMode(newAlignment)
+  }
+
+  return (
+    <ToggleButtonGroup
+      color="primary"
+      value={calcMode}
+      exclusive
+      onChange={handleChange}
+      aria-label="Platform"
+      sx={{ mt: 4 }}
+    >
+      <ToggleButton value="decision">결정력 계산기</ToggleButton>
+      <ToggleButton value="stamina">내구력 계산기</ToggleButton>
+    </ToggleButtonGroup>
+  )
 })
