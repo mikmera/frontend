2d4b3b1eda2399e713ed536a9944c6c2c5e06b9f
rihayoru <qtcutie@kakao.com>
2023-01-20 16:38:12 +0900

    feat: add firebase app check

--- src/util.ts
+++ src/util.ts
@@ -1,11 +1,29 @@
 import Axios from 'axios'
+import {
+  initializeAppCheck,
+  getToken,
+  ReCaptchaV3Provider,
+} from 'firebase/app-check'
+import { app } from '~/service/firebase'
+
+const appCheck = initializeAppCheck(app, {
+  provider: new ReCaptchaV3Provider('6LcPWBEkAAAAAGvMehTfPWRw4-yqyAJ9mNg28xRp'),
+})
 
 export const api = Axios.create({
   baseURL: import.meta.env.VITE_API_BASE,
 })
 
 export const fetcher = async (url: string) => {
-  const { data } = await api.get(url)
+  const appCheckTokenResponse = await getToken(
+    appCheck,
+    /* forceRefresh= */ false
+  )
+  const { data } = await api.get(url, {
+    headers: {
+      'X-Firebase-AppCheck': appCheckTokenResponse.token,
+    },
+  })
 
   return data.body
 }
