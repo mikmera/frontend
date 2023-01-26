bc5bd014663f61c629a9ac7826b77c1db8d8ea5e
rihayoru <qtcutie@kakao.com>
2023-01-26 18:27:36 +0900

    feat: add monitoring

--- src/main.tsx
+++ src/main.tsx
@@ -13,8 +13,10 @@ import {
   Tooltip,
   Legend,
 } from 'chart.js'
-import { getCookie } from 'react-use-cookie'
 import './global.scss'
+import { getCookie } from 'react-use-cookie'
+import * as Sentry from '@sentry/react'
+import { BrowserTracing } from '@sentry/tracing'
 import { ThemeContext } from './context'
 import { ThemeProvider, createTheme } from '@mui/material/styles'
 import { SnackbarProvider } from 'notistack'
@@ -64,6 +66,12 @@ export default function ToggleColorMode() {
     </ThemeContext.Provider>
   )
 }
+
+Sentry.init({
+  dsn: 'https://b241ea8ae74d41c8a6314dae36afdff2@o4504570083016704.ingest.sentry.io/4504570084917248',
+  integrations: [new BrowserTracing()],
+  tracesSampleRate: 1.0,
+})
 
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend)
 
